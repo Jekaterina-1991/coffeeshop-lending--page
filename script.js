88a114f3943530bfed54ed7d56ac1c6647f7634e
Jekaterina-1991 <jekaterina.kljukina@gmail.com>
2023-09-19 21:04:16 +0200

    changes

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const starbucksImg = document.querySelector(".starbucks");
+  const circle = document.querySelector(".circle");
+  const thumbImages = document.querySelectorAll(".thumb img");
+
+  // Add a click event listener to each thumbnail image
+  thumbImages.forEach((thumbImage) => {
+    thumbImage.addEventListener("click", function () {
+      const imageSrc = thumbImage.getAttribute("data-image");
+      const circleColor = thumbImage.getAttribute("data-color");
+
+      imgSlider(imageSrc);
+      changeCircleColor(circleColor);
+    });
+  });
+
+  function imgSlider(image) {
+    starbucksImg.src = image;
+  }
+
+  function changeCircleColor(color) {
+    circle.style.background = color;
+  }
+});
